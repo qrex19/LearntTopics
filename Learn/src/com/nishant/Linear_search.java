27abea50b61bd2ea27b469f8aaaac556ef3f9576
qrex19 <nishanttjhaa25@gmail.com>
2022-05-10 00:49:10 +0530

    fixed a bug in linear search code(for loop)

--- Learn/src/com/nishant/Linear_search.java
+++ Learn/src/com/nishant/Linear_search.java
@@ -4,11 +4,11 @@ public class Linear_search {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-        System.out.println(linearsearch(arr, 60));
+        System.out.println(linearsearch(arr, 1234));
     }
 
     static int linearsearch(int[] arr, int target){
-        for (int i = 0; i < target; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if(target == arr[i]){
                 return i;
             }
